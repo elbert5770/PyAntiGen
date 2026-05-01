5abb19d0886c9a0be4c094dace3f38262b14c020
Don Elbert <40698678+elbert5770@users.noreply.github.com>
2026-05-01 13:28:38 -0700

    Bump version from 0.1.0 to 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyantigen',
-    version='0.1.0',
+    version='1.0',
     description='A declarative framework for building compartmental Antimony models',
     author='Open Source Contributor',
     packages=find_packages(include=['framework*']),
