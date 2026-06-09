5c80afa9b2a71b813ab35e8156ae3305b2910063
elbert5770 <elbert5770@gmail.com>
2026-06-09 09:06:05 -0700

    Only write SBML when requested

--- framework/TelluriumGen.py
+++ framework/TelluriumGen.py
@@ -1,14 +1,15 @@
 import tellurium as te
 from framework.antimony_utils import archive_antimony_snapshot
 
-def TelluriumGen(model_text, paths ):
+def TelluriumGen(model_text, paths, settings=None ):
     print("Loading model into Tellurium...")
     try:
         r = te.loada(model_text)
     except Exception as e:
         raise RuntimeError(f"Error loading model: {e}") from e
 
-    sbml_content = r.getSBML()
-    archive_dir = archive_antimony_snapshot(paths["MODEL_NAME"], paths["repo_root"], sbml_content=sbml_content)
-    print(f"Archive and SBML written to: {archive_dir}")
+    if settings["save_SBML?"]:
+        sbml_content = r.getSBML()
+        archive_dir = archive_antimony_snapshot(paths["MODEL_NAME"], paths["repo_root"], sbml_content=sbml_content)
+        print(f"Archive and SBML written to: {archive_dir}")
     return r
