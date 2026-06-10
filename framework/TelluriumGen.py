21dd29ee7dbdc6f45f693c1e608b67b390db09a8
elbert5770 <elbert5770@gmail.com>
2026-06-09 20:40:05 -0700

    fix settings=None

--- framework/TelluriumGen.py
+++ framework/TelluriumGen.py
@@ -8,8 +8,9 @@ def TelluriumGen(model_text, paths, settings=None ):
     except Exception as e:
         raise RuntimeError(f"Error loading model: {e}") from e
 
-    if settings["save_SBML?"]:
-        sbml_content = r.getSBML()
-        archive_dir = archive_antimony_snapshot(paths["MODEL_NAME"], paths["repo_root"], sbml_content=sbml_content)
-        print(f"Archive and SBML written to: {archive_dir}")
+    if settings is not None:
+        if settings["save_SBML?"]:
+            sbml_content = r.getSBML()
+            archive_dir = archive_antimony_snapshot(paths["MODEL_NAME"], paths["repo_root"], sbml_content=sbml_content)
+            print(f"Archive and SBML written to: {archive_dir}")
     return r
